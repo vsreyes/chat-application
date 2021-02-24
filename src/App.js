f60c572ef9e8767e203f345801da2f3622fcc54b
vsreyes <virgilio.s.reyes@gmail.com>
2021-02-24 17:59:51 +0100

    Chat setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height='100vh'
+      projectID='529f400b-aaa0-4d87-85b6-82a2671e2516'
+      userName='vsreyes'
+      userSecret='123456'
+    />
+  );
+};
+
+export default App;
